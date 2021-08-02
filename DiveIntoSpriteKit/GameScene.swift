cb84470a025f26b13749ded3217088be738bfe3e
sans-connaissance <davemalicke@gmail.com>
2021-08-02 19:51:33 -0400

    Added touch controls

--- DiveIntoSpriteKit/GameScene.swift
+++ DiveIntoSpriteKit/GameScene.swift
@@ -13,6 +13,7 @@ import SpriteKit
 class GameScene: SKScene {
     
     let player = SKSpriteNode(imageNamed: "player-motorbike")
+    var touchingPlayer = false
     
     
     override func didMove(to view: SKView) {
@@ -36,10 +37,33 @@ class GameScene: SKScene {
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         // this method is called when the user touches the screen
+        
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
+        
+        if tappedNodes.contains(player) {
+            touchingPlayer = true
+        }
+        
     }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        guard touchingPlayer else { return }
+        guard let touch = touches.first else { return }
+        
+        let location = touch.location(in: self)
+        player.position = location
+        
+        
+    }
+    
 
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         // this method is called when the user stops touching the screen
+        touchingPlayer = false
+        
     }
 
     override func update(_ currentTime: TimeInterval) {
